e6e847ea05b27a5b1d18dd246d735bdbff6f556f
barbarIQ <rihalsulaiman@gmail.com>
2025-10-28 20:47:25 +0700

    feat(database): update DBMigrate function to include specific models for migration

--- backend/database/database.go
+++ backend/database/database.go
@@ -28,7 +28,7 @@ func DBLoad() {
 }
 
 func DBMigrate() {
-	if err := DB.Debug().AutoMigrate(); err != nil {
+	if err := DB.Debug().AutoMigrate(&models.Bookmark{}, models.Chapter{}, models.Genre{}, models.Novel{}, models.User{}); err != nil {
 		panic("Failed to migrate database")
 	}
 	fmt.Println("Database migration completed")
