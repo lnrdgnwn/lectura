9d8d249cff8678cd0e12e060629a9065322e8ac1
barbarIQ <rihalsulaiman@gmail.com>
2025-11-01 14:51:06 +0700

    fix(bookmark_routes): reorder middleware and controller in route definitions

--- backend/routes/bookmark_routes.go
+++ backend/routes/bookmark_routes.go
@@ -9,11 +9,11 @@ import (
 
 func bookmarkRoutes(api fiber.Router) {
 	// tambahkan bookmark (user harus login)
-	api.Post("/bookmarks", controllers.AddBookmark, middlewares.AuthMiddleware())
+	api.Post("/bookmarks",  middlewares.AuthMiddleware(), controllers.AddBookmark)
 
 	// hapus bookmark untuk novel tertentu (user harus login)
-	api.Delete("/bookmarks/:novel_id", controllers.RemoveBookmark, middlewares.AuthMiddleware())
+	api.Delete("/bookmarks/:novel_id", middlewares.AuthMiddleware(), controllers.RemoveBookmark)
 
 	// daftar bookmark user yang sedang login
-	api.Get("/users/me/bookmarks", controllers.ListBookmarks, middlewares.AuthMiddleware())
+	api.Get("/users/me/bookmarks",  middlewares.AuthMiddleware(), controllers.ListBookmarks)
 }
