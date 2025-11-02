97730289f5dfbcdb77d37ea64f8551e7841e13a4
barbarIQ <rihalsulaiman@gmail.com>
2025-11-02 17:40:48 +0700

    feat(database): update database.go add Redis connection and update migration for RefreshTokens

--- backend/database/database.go
+++ backend/database/database.go
@@ -1,15 +1,19 @@
 package database
 
 import (
+	"context"
+	"strconv"
 	"fmt"
 	"os"
 	"final_project/models"
 
+	"github.com/redis/go-redis/v9"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
+var Rdb *redis.Client
 
 func DBLoad() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
@@ -27,8 +31,28 @@ func DBLoad() {
 	DB = db
 }
 
+func Redis() {
+	ctx := context.Background()
+	dbNum, err := strconv.Atoi(os.Getenv("REDIS_DB"))
+	if err != nil {
+		dbNum = 0
+	}
+
+	Rdb = redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       dbNum,
+	})
+
+	pong, err := Rdb.Ping(ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Redis connected:", pong)
+}
+
 func DBMigrate() {
-	if err := DB.Debug().AutoMigrate(&models.Bookmark{}, models.Chapter{}, models.Genre{}, models.Novel{}, models.User{}, models.Tag{},models.RefreshToken{}); err != nil {
+	if err := DB.Debug().AutoMigrate(&models.Bookmark{}, models.Chapter{}, models.Genre{}, models.Novel{}, models.User{}, models.Tag{},models.RefreshTokens{}); err != nil {
 		panic("Failed to migrate database")
 	}
 	fmt.Println("Database migration completed")
