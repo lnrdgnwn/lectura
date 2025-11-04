12702381db22fa67987d314b4ed7de59e390e394
barbarIQ <rihalsulaiman@gmail.com>
2025-11-04 22:24:26 +0700

    refactor(chapter_models): add index to PublishedAt field for improved query performance

--- backend/models/chapter_models.go
+++ backend/models/chapter_models.go
@@ -10,7 +10,7 @@ type Chapter struct {
 	OrderNo     int        `gorm:"not null" json:"order_no"`
 	Title       *string    `gorm:"size:255" json:"title,omitempty"`
 	Content     *string    `gorm:"type:mediumtext" json:"content,omitempty"`
-	PublishedAt *time.Time `json:"published_at,omitempty"`
+	PublishedAt *time.Time `gorm:"index" json:"published_at,omitempty"`
 	CreatedAt   time.Time  `json:"created_at"`
 	UpdatedAt   time.Time  `json:"updated_at"`
 }
