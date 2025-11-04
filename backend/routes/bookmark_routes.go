0bbe6cd8c2fe4b5a8c9d60beec7928eb66c9e5c5
barbarIQ <rihalsulaiman@gmail.com>
2025-11-04 11:52:18 +0700

    refactor(bookmark_routes): remove comments for improved clarity in bookmarkRoutes function

--- backend/routes/bookmark_routes.go
+++ backend/routes/bookmark_routes.go
@@ -8,12 +8,7 @@ import (
 )
 
 func bookmarkRoutes(api fiber.Router) {
-	// tambahkan bookmark (user harus login)
 	api.Post("/bookmarks",  middlewares.AuthMiddleware(), controllers.AddBookmark)
-
-	// hapus bookmark untuk novel tertentu (user harus login)
 	api.Delete("/bookmarks/:novel_id", middlewares.AuthMiddleware(), controllers.RemoveBookmark)
-
-	// daftar bookmark user yang sedang login
 	api.Get("/users/me/bookmarks",  middlewares.AuthMiddleware(), controllers.ListBookmarks)
 }
